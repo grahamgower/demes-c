309fb5770e6468936fcaf83a7f15810e4c41bb3c
Graham Gower <graham.gower@gmail.com>
2021-05-15 16:06:02 +0200

    Substantially expand the test cases.

--- tests/tests.sh
+++ tests/tests.sh
@@ -9,10 +9,18 @@ die() {
 }
 export -f die
 
+resolve() {
+    yaml=$1
+    $RESOLVE $yaml > /dev/null
+    if [ $? != 0 ]; then
+        die "$yaml: failed to resolve valid model"
+    fi
+}
+export -f resolve
+
 # Check that the fully-resolved output for a model is resolved identically.
 idempotent() {
     yaml=$1
-    # echo "idempotent: $yaml"
     sum1=$($RESOLVE $yaml | md5sum | cut -f1 -d" ")
     sum2=$($RESOLVE $yaml | $RESOLVE /dev/stdin | md5sum | cut -f1 -d" ")
     if [ $sum1 != $sum2 ]; then
@@ -24,7 +32,6 @@ export -f idempotent
 # Check that the choice of locale doesn't alter the output.
 locale_independent() {
     yaml=$1
-    # echo "locale_independent: $yaml"
     sum1=$(LANG=C $RESOLVE $yaml | md5sum | cut -f1 -d" ")
     sum2=$(LANG=en_DK.UTF-8 $RESOLVE $yaml | md5sum | cut -f1 -d" ")
     if [ $sum1 != $sum2 ]; then
@@ -33,27 +40,40 @@ locale_independent() {
 }
 export -f locale_independent
 
-find input/valid -name \*.yaml -print0 \
-    | xargs -0 -n1 $RESOLVE \
-    >/dev/null \
-    || die "resolve valid"
+assert_failure() {
+    yaml=$1
+    $RESOLVE $yaml > /dev/null 2>&1
+    if [ $? = 0 ]; then
+        die "$yaml: failed to reject invalid model"
+    fi
+}
+export -f assert_failure
+
+mocked_resolve() {
+    yaml=$1
+    $MOCKED_RESOLVE $yaml > /dev/null 2>&1
+    if [ $? != 0 ]; then
+        die "$yaml: mocked resolve failed"
+    fi
+}
+export -f mocked_resolve
 
 find input/valid -name \*.yaml -print0 \
-    | xargs -0 -n1 xargs -0 -n1 bash -c 'idempotent "$@"' bash \
-    >/dev/null \
-    || die "resolve valid"
+    | xargs -0 -n1 bash -c 'resolve "$@"' bash \
+    || exit 1
 
 find input/valid -name \*.yaml -print0 \
-    | xargs -0 -n1 xargs -0 -n1 bash -c 'locale_independent "$@"' bash \
-    >/dev/null \
-    || die "resolve valid"
+    | xargs -0 -n1 bash -c 'idempotent "$@"' bash \
+    || exit 1
+
+find input/valid -name \*.yaml -print0 \
+    | xargs -0 -n1 bash -c 'locale_independent "$@"' bash \
+    || exit 1
 
 find input/invalid -name \*.yaml -print0 \
-    | xargs -0 -n1 $RESOLVE \
-    >/dev/null 2>&1 \
-    && die "resolve invalid"
+    | xargs -0 -n1 bash -c 'assert_failure "$@"' bash \
+    || exit 1
 
 find input/valid -name \*.yaml -print0 \
-    | xargs -0 -n1 $MOCKED_RESOLVE \
-    >/dev/null 2>&1 \
-    || die "mocked-resolve valid"
+    | xargs -0 -n1 bash -c 'mocked_resolve "$@"' bash \
+    || exit 1
