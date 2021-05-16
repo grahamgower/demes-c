ad240f35ce86f6ef436cd53a790d93d932288591
Graham Gower <graham.gower@gmail.com>
2021-05-16 09:07:42 +0200

    mock newlocale()/uselocale()

--- tests/mockfail.c
+++ tests/mockfail.c
@@ -6,11 +6,14 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <errno.h>
 
 static void *(*__real_malloc)(size_t) = NULL;
 static void *(*__real_realloc)(void *, size_t) = NULL;
 static void *(*__real_calloc)(size_t, size_t) = NULL;
+static locale_t (*__real_newlocale)(int, const char *, locale_t) = NULL;
+static locale_t (*__real_uselocale)(locale_t) = NULL;
 
 size_t fail_after = -1;
 size_t counter = 0;
@@ -29,6 +32,14 @@ mockinit()
     }
     if (!(__real_calloc = dlsym(RTLD_NEXT, "calloc"))) {
         fprintf(stderr, "mockfail: dlsym: calloc: %s\n", dlerror());
+        abort();
+    }
+    if (!(__real_newlocale = dlsym(RTLD_NEXT, "newlocale"))) {
+        fprintf(stderr, "mockfail: dlsym: newlocale: %s\n", dlerror());
+        abort();
+    }
+    if (!(__real_uselocale = dlsym(RTLD_NEXT, "uselocale"))) {
+        fprintf(stderr, "mockfail: dlsym: uselocale: %s\n", dlerror());
         abort();
     }
 }
@@ -66,3 +77,24 @@ realloc(void* ptr, size_t size)
     }
 }
 
+locale_t
+newlocale(int category_mask, const char *locale, locale_t base)
+{
+    if (++counter > fail_after) {
+        errno = ENOMEM;
+        return (locale_t)0;
+    } else {
+        return __real_newlocale(category_mask, locale, base);
+    }
+}
+
+locale_t
+uselocale(locale_t newloc)
+{
+    if (++counter > fail_after) {
+        errno = EINVAL;
+        return (locale_t)0;
+    } else {
+        return __real_uselocale(newloc);
+    }
+}
